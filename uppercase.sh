283135f82bc93331b28a094180b1cca4af9e3ea6
Abithhaa <159014443+Abithhaa@users.noreply.github.com>
2024-03-14 15:47:40 +0530

    Create uppercase.sh

---
+++ uppercase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <hello.txt>"
+    exit 1
+fi
+
+
+if [ ! -f "$1" ]; then
+    echo "Error: File '$1' not found."
+    exit 1
+fi
+
+
+while IFS= read -r line; do
+    echo "$line" | tr '[:lower:]' '[:upper:]'
+done < "$1"
+
+echo "Contents of $1 displayed in uppercase."
