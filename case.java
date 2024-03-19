ea83ebca5327e1d13a6bdec4289dd60717b6eb4c
Abithhaa <159014443+Abithhaa@users.noreply.github.com>
2024-03-19 15:41:22 +0530

    Delete case.java

--- case.java
+++
@@ -1,9 +0,0 @@
-class StartString {
-public static void main(String[]args) {
-String s1 = "Good Morning";
-System.out.println(s1);
-
-System.out.println("To uppercase:"+s1.toUpperCase());
-System.out.println("To lowercase:"+s1.toLowerCase());
-}
-}
